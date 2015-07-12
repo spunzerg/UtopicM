750f0898f13045f62feb7154e2161f1befb7f1b7
spun <neoappengine@gmail.com>
2015-07-12 12:41:08 +0200

    socket.io

---
+++ gameServer.js
@@ -0,0 +1,10 @@
+var gameServer = function (io) {
+	io.on('connection', function (socket) {
+		socket.emit('news', { hello: 'world' });
+		socket.on('my other event', function (data) {
+			console.log(data);
+		});
+	});
+}
+
+module.exports = gameServer;
