2f551ef789578bc839a0587774e300ddbe39e4cd
Víctor Manuel González Mira <shadowfiltro@gmail.com>
2015-07-14 21:07:10 +0200

    Clase Player
    - Separado el jugador en una clase aparte

--- src/js/level1.ts
+++ src/js/level1.ts
@@ -1,38 +1,37 @@
 module UtopicM {
+  declare var io;
 
   export class Level1 extends Phaser.State {
-    constructor() {
-      super();
-    }
+
     textValue: Phaser.Text;
     updateCount: number;
-    downKey;
-    upKey;
-    leftKey;
-    rightKey;
-    player;
+    player: UtopicM.Player;
+    socket;
 
     preload() {
 
     }
 
     create() {
+      this.socket = io();
+      this.socket=io();
       var style = { font: "65px Arial", fill: "#ff0000", align: "center" };
       this.textValue = this.game.add.text(0, 0, "0", style);
       this.updateCount = 0;
-      this.player = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'player');
-      this.player.anchor.setTo(0.5, 0.5)
+      this.player = new Player(this.game, 130, 284);
+      //this.player = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'player');
+      //this.player.anchor.setTo(0.5, 0.5)
 
       //Entradas de teclado
-      this.upKey = this.game.input.keyboard.addKey(Phaser.Keyboard.UP);
+      /*this.upKey = this.game.input.keyboard.addKey(Phaser.Keyboard.UP);
       this.downKey = this.game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
       this.leftKey = this.game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
-      this.rightKey = this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+      this.rightKey = this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);*/
     }
 
     update() {
       this.textValue.text = (this.updateCount++).toString();
-
+      /*
       if (this.upKey.isDown) {
         this.player.y -= 2;
       } else if (this.downKey.isDown) {
@@ -42,7 +41,7 @@ module UtopicM {
         this.player.x -= 2;
       } else if (this.rightKey.isDown) {
         this.player.x += 2;
-      }
+      }*/
     }
 
     render() {
--- src/js/player.ts
+++ src/js/player.ts
@@ -1,9 +1,37 @@
-export class Player {
-  game: Phaser.Game;
-  playerSprite: Phaser.Sprite;
+module UtopicM {
+  export class Player extends Phaser.Sprite {
+    game: Phaser.Game;
+    playerSprite: Phaser.Sprite;
+    downKey;
+    upKey;
+    leftKey;
+    rightKey;
 
-  constructor(game) {
-    this.game=game;
-    //this.playerSprite = this.game.add.sprite();
+    constructor(game: Phaser.Game, x: number, y: number) {
+      super(game, x, y, 'player', 0);
+      this.anchor.setTo(0.5, 0);
+      game.add.existing(this);
+
+      this.upKey = this.game.input.keyboard.addKey(Phaser.Keyboard.UP);
+      this.downKey = this.game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+      this.leftKey = this.game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+      this.rightKey = this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+    }
+
+
+
+    update() {
+      if (this.upKey.isDown) {
+        this.y -= 2;
+      } else if (this.downKey.isDown) {
+        this.y += 2;
+      }
+      if (this.leftKey.isDown) {
+        this.x -= 2;
+      } else if (this.rightKey.isDown) {
+        this.x += 2;
+      }
+    }
+
   }
 }
